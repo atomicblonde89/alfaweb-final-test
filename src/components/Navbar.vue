7a8f1e1c532083db3dc2480a4cb459ef658fde08
Demian <learc47@gmail.com>
2022-06-18 23:19:01 -0400

    init w. components

---
+++ src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-navbar toggleable type="dark" variant="dark" class="d-flex">
+    <b-navbar-toggle class="m-2" target="navbar-toggle-collapse">
+      <template #default="{ expanded }">
+        <b-icon v-if="expanded" icon="chevron-bar-up"></b-icon>
+        <b-icon v-else icon="border-width"></b-icon>
+      </template>
+    </b-navbar-toggle>
+    <b-collapse id="navbar-toggle-collapse" is-nav class="m-2">
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item to="/admin">Admin</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+    <b-navbar-brand to="/">Cursos AlfaWeb</b-navbar-brand>
+    <UserLogin />
+  </b-navbar>
+</template>
+
+<script>
+import UserLogin from "@/components/UserLogin";
+
+export default {
+  name: "Navbar",
+  components: {
+    UserLogin,
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/UserLogin.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="text-white m-2">
+    <div v-if="userLogin === null" @click="ingresar">
+      <label>Ingresar</label>
+      <b-icon icon="arrow-right-circle-fill"></b-icon>
+    </div>
+    <div v-else @click="salir">
+      {{ userLogin }}
+      <b-icon icon="arrow-left-circle-fill"></b-icon>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapState } from "vuex";
+
+export default {
+  computed: {
+    ...mapState(["userLogin"]),
+  },
+  mounted() {
+    this.getUserLogin();
+  },
+  methods: {
+    ...mapActions(["getUserLogin", "logoutUser"]),
+    async salir() {
+      await this.logoutUser();
+      this.$router.push("/ingresar");
+    },
+    ingresar() {
+      this.$router.push("/ingresar");
+    },
+  },
+};
+</script>
+
+<style></style>
